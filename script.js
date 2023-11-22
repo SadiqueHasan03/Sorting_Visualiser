4b77afc7bfa75575e863476d97008417d34eeeab
Sadique Hasan <sadiquehasan03@gmail.com>
2023-11-22 19:55:02 +0530

    swap highlighting added

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-const n = 10;
+const n = 20;
 const array = [];
 
-for(let i = 0; i<n; i++){
-    array[i] = Math.random();
+init();
+
+function init(){
+    for(let i = 0; i<n; i++){
+        array[i] = Math.random();
+    }
+    showBars();
 }
 
-do{
-    var swapped = false;
-    for(let i = 1; i < array.length;i++){
-        if(array[i-1] > array[i]){
-            swapped = true;
-            [array[i-1], array[i]] = [array[i], array[i-1]];
+function play(){
+    const copy = [...array];
+    const swaps = bubbleSort(copy);
+    animate(swaps);
+}
 
-        }
+function animate(swaps) {
+    if(swaps.length==0){
+        showBars(); 
+        return;
     }
-}while(swapped)
+    const [i,j] = swaps.shift();
+    [array[i],array[j]] = [array[j], array[i]];
+    showBars([i,j]);
+    setTimeout(function() {
+        animate(swaps);
+    },200);
+}
 
 
-for(let i = 0; i<array.length ;i++){
-    const bar = document.createElement("div");
-    bar.style.height = array[i]*100 + "%";
-    bar.classList.add("bar");
-    container.appendChild(bar);
+function bubbleSort(array){
+    const swaps = [];
+    do{
+        var swapped = false;
+        for(let i = 1; i < array.length;i++){
+            if(array[i-1] > array[i]){
+                swapped = true;
+                swaps.push([i-1,i]);
+                [array[i-1], array[i]] = [array[i], array[i-1]];
+    
+            }
+        }
+    }while(swapped);
+    return swaps;
+}
+
+function showBars(indices){
+    container.innerHTML="";
+    for(let i = 0; i<array.length ;i++){
+        const bar = document.createElement("div");
+        bar.style.height = array[i]*100 + "%";
+        bar.classList.add("bar");
+
+        if(indices && indices.includes(i)){
+            bar.style.backgroundColor="red";
+        }
+        container.appendChild(bar);
+    }
 }
