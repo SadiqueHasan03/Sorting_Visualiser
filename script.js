3ed9aa9ac2ed107ad1d14f80fc7ac422b72f4d42
Sadique Hasan <143483928+SadiqueHasan03@users.noreply.github.com>
2025-04-04 21:12:01 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -467,3 +467,33 @@ function copyCode() {
     });
 }
 
+
+// background animation logic
+particlesJS('particles-js', {
+    particles: {
+        number: { value: 80 },
+        color: { value: '#ffffff' },
+        shape: { type: 'circle' },
+        opacity: { value: 0.5 },
+        size: { value: 3 },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+        }
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        events: {
+            onhover: { enable: true, mode: 'repulse' },
+            onclick: { enable: true, mode: 'push' },
+            resize: true
+        }
+    },
+    retina_detect: true
+});
+
